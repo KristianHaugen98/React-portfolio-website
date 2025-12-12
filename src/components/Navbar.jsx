1a320a15c7bc38d8adc39559079b0edd0f737ea2
Kristian Haugen <kristian-haugen21@live.no>
2025-12-12 13:39:14 +0100

    Update Navbar.jsx
    Fixed so the the hambuer menu is working as it should (when using mobile).

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,7 +53,11 @@ export const NavBar = () => {
         </div>
         {/* mobile nav*/}
 
-        <button onClick={() => setIsMenuOpen((prev) => !prev)}>
+        <button
+          onClick={() => setIsMenuOpen((prev) => !prev)}
+          className="md:hidden p-2 text-foreground z-50"
+          aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}
+        >
           {" "}
           {isMenuOpen ? <X size={24} /> : <Menu size={24} />}{" "}
         </button>
